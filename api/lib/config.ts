bc9d1977700d625c46cee346b4f8fdfb50593d50
Stanislaw <staszekmendel1029@gmail.com>
2024-04-11 14:26:12 +0200

    Zad 7

--- api/lib/config.ts
+++ api/lib/config.ts
@@ -1,3 +1,6 @@
+//config.ts
+
 export const config = {
     port: process.env.PORT || 3100
  };
+ 
--- api/lib/controllers/data.controller.ts
+++ api/lib/controllers/data.controller.ts
@@ -1,3 +1,5 @@
+//data.controller.ts
+
 import Controller from '../interfaces/controller.interface';
 import { Request, Response, NextFunction, Router } from 'express';
 
@@ -14,6 +16,12 @@ class DataController {
     private initializeRoutes() {
         this.router.get(`${this.path}/all`, this.getAll);
         this.router.post(`${this.path}/add`, this.addData);
+        this.router.get(`${this.path}/:id`, this.getPostById);
+        this.router.post(`${this.path}`, this.addPost);
+        this.router.delete(`${this.path}/:id`, this.deletePostById);
+        this.router.post(`${this.path}/:num`, this.getNPosts);
+        this.router.get(`${this.path}s`, this.getAllPosts);
+        this.router.delete(`${this.path}s`, this.deleteAllPosts);
     }
 
     private getAll = async (request: Request, response: Response, next: NextFunction) => {
@@ -26,9 +34,69 @@ class DataController {
 
     private addData = async (request: Request, response: Response, next: NextFunction) => {
         try {
+            console.log(request.body); // Dodajemy ten log
+            const elem = request.body.elem;
+            if (elem === undefined || elem === null) {
+                throw new Error('Brak wartości "elem" w ciele żądania.');
+            }
+            testArr.push(elem);
+            response.status(200).json(testArr);
+        } catch (error) {
+            next(error);
+        }
+    };
+    private getPostById = async (request: Request, response: Response, next: NextFunction) => {
+        try {
+            const id = parseInt(request.params.id);
+            const post = testArr[id];
+            response.status(200).json(post);
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    private addPost = async (request: Request, response: Response, next: NextFunction) => {
+        try {
             const { elem } = request.body;
             testArr.push(elem);
             response.status(200).json(testArr);
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    private deletePostById = async (request: Request, response: Response, next: NextFunction) => {
+        try {
+            const id = parseInt(request.params.id);
+            testArr.splice(id, 1);
+            response.status(200).json(testArr);
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    private getNPosts = async (request: Request, response: Response, next: NextFunction) => {
+        try {
+            const num = parseInt(request.params.num);
+            const posts = testArr.slice(0, num);
+            response.status(200).json(posts);
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    private getAllPosts = async (request: Request, response: Response, next: NextFunction) => {
+        try {
+            response.status(200).json(testArr);
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    private deleteAllPosts = async (request: Request, response: Response, next: NextFunction) => {
+        try {
+            testArr = [];
+            response.status(200).json([]);
         } catch (error) {
             next(error);
         }
